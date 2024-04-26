206e7107d8c6193257699a4130bc200100249e18
Yashraj Shah <yashrajshah28@gmail.com>
2024-04-27 00:26:00 +0530

    small updates

--- frontend/src/shared/ContactUs.jsx
+++ frontend/src/shared/ContactUs.jsx
@@ -1,41 +1,38 @@
-import React from 'react';
-import { useForm, ValidationError } from '@formspree/react';
-import './contactus.css'; // Import the CSS file
+import React from "react";
+import { useForm, ValidationError } from "@formspree/react";
+import "./contactus.css"; // Import the CSS file
+import connect from "../assets/images/connect.png";
+import Subtitle from './../shared/subtitle'
 
 function ContactForm() {
   const [state, handleSubmit] = useForm("xrgnrwjj");
-  
+
   if (state.succeeded) {
-    return <h1><p className="success-message">Thanks for joining!</p></h1>;
+    return (
+      <h1>
+        <p className="success-message">Thanks for joining!</p>
+      </h1>
+    );
   }
-  
+
   return (
     <div className="form-container">
+      <div className='form-container__img'>
+      <img src={connect} alt="" />
+      </div>
       <form onSubmit={handleSubmit}>
-        <h1 className='contact'>Contact Us</h1>
-        <h3 className='contact'>If any enquires</h3>
+      <Subtitle subtitle={'Contact Us'} />
+        {/* <h1 className="contact">Contact Us</h1> */}
+        <h4 className="contact">If any enquires</h4>
         <label className="form-label" htmlFor="email">
           Email Address
         </label>
-        <input
-          id="email"
-          type="email"
-          name="email"
-          className="form-input"
-        />
-        <ValidationError
-          prefix="Email"
-          field="email"
-          errors={state.errors}
-        />
+        <input id="email" type="email" name="email" className="form-input" />
+        <ValidationError prefix="Email" field="email" errors={state.errors} />
         <label className="form-label" htmlFor="message">
           Message
         </label>
-        <textarea
-          id="message"
-          name="message"
-          className="form-textarea"
-        />
+        <textarea id="message" name="message" className="form-textarea" />
         <ValidationError
           prefix="Message"
           field="message"
@@ -50,6 +47,7 @@ function ContactForm() {
         </button>
       </form>
     </div>
+    
   );
 }
 
--- frontend/src/shared/Newsletter.jsx
+++ frontend/src/shared/Newsletter.jsx
@@ -29,6 +29,7 @@ const NewsLetter = () => {
             </Row>
          </Container>
       </section>
+      
    )
 }
 
